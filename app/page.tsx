2c511f03e1efad837132767696aba45448ed8f8f
badzso-boop <norbert.ujj@gmail.com>
2024-08-01 22:20:35 +0200

    frontend plus

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,16 @@ import downArrow from "./_Assets/down-arrow.png";
 import creator from "./_Assets/Website Creator-rafiki.png";
 import charts from "./_Assets/Charts-pana.png";
 import arrow from "./_Assets/arrow.png";
+import team from "./_Assets/Team goals-rafiki.png";
+import User from "./_Assets/User research-bro.png";
+
+import gyorsasag from "./_Assets/gyorsasag.png";
+import alacsony_ar from "./_Assets/alacsony_ar.png";
+import megbizhatosag from "./_Assets/megbizhatosag.png";
+import pontossag from "./_Assets/pontossag.png";
+
+import pipa from "./_Assets/pipa.png";
+import iksz from "./_Assets/iksz.png";
 
 export default function Home() {
   return (
@@ -66,7 +76,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full flex flex-col md:flex-row md:h-[450px] md:mt-0 border">
+      <div className="w-full flex flex-col md:flex-row md:h-[450px] md:mt-0">
         <div className="w-full md:w-1/2 flex items-center justify-center">
           <div className="rounded-full bg-gradient-to-br from-customStart to-customEnd w-2/3">
             <Image
@@ -103,11 +113,224 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-center items-center mt-[35px]">
+      <div className="text-center items-center mt-[35px] p-4">
         <h3 className="text-2xl md:text-3xl font-bold m-3">
           Miért válassz minket?
         </h3>
-        <p>Mert</p>
+        <div className="flex flex-wrap justify-center">
+          <div className="items-center justify-center flex flex-col mx-7 md:mx-12">
+            <Image src={pontossag} alt="pontossag" width={65} className="m-3" />
+            <span className="font-bold">Pontosság</span>
+          </div>
+          <div className="items-center justify-center flex flex-col mx-7 md:mx-12">
+            <Image src={gyorsasag} alt="gyorsasag" width={65} className="m-3" />
+            <span className="font-bold">Gyorsaság</span>
+          </div>
+          <div className="items-center justify-center flex flex-col mx-7 md:mx-12">
+            <Image
+              src={alacsony_ar}
+              alt="alacsony_ar"
+              width={65}
+              className="m-3"
+            />
+            <span className="font-bold">Alacsony ár</span>
+          </div>
+          <div className="items-center justify-center flex flex-col mx-7 md:mx-12">
+            <Image
+              src={megbizhatosag}
+              alt="megbizhatosag"
+              width={65}
+              className="m-3"
+            />
+            <span className="font-bold">Megbízhatóság</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col md:flex-row md:h-[450px] bg-cyellow bg-opacity-10 p-10">
+        <div className="w-full md:w-1/2 flex p-4 flex-wrap">
+          <div>
+            <h6 className="text-cyellow uppercase ml-3">Egyedi ajánlat!</h6>
+            <h3 className="text-2xl md:text-3xl font-bold m-3">
+              Webfejlesztés szinte ingyen!
+            </h3>
+            <p className="text-base md:text-lg ml-3 leading-5">
+              Az N&M Web elindulása alkalmából teljesen ingyenes fejlesztési
+              költségi akciót hírdetünk. Célünk, hogy{" "}
+              <span className="text-cyellow">együtt növekedjünk.</span> Így az
+              első 3 ügyfélnek , fejlesztési költségektől mentesen készítünk
+              weboldalt.
+            </p>
+            <p className="text-cyellow font-bold ml-3 text-xl">
+              Ne hagyd ki ezt az egyedi ajánlatot, és kérj ajánlatot még ma!
+            </p>
+          </div>
+          <div className="ml-3 mt-3">
+            <span
+              className={`${lato.className} bg-gradient-to-br from-customStart to-customEnd p-2 rounded-full text-white px-3 py-2 md:px-4 md:py-3 mt-3 uppercase text-xs md:text-sm font-bold`}
+            >
+              Ajánlatot kérek
+            </span>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 flex p-2">
+          <Image
+            src={team}
+            alt="logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="text-center items-center mt-[35px] p-4">
+        <h3 className="text-2xl md:text-3xl font-bold m-3 uppercase">
+          Lássuk milyen csomagok közül válogathatsz
+        </h3>
+        <p>Hogy megtaláld számodra a legjobbat.</p>
+
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="items-center justify-center flex flex-col m-4 border rounded-2xl p-3 h-[500px] w-[270px]">
+            <h4>Statikus weboldal</h4>
+            <p>Egyszerű weboldal, statikus tartalommal</p>
+            <div className="border rounded-2xl bg-cyellow bg-opacity-20 w-3/4 mx-2 my-4 p-3 h-[329px] w-[245px] flex flex-col justify-between">
+              <ul className="my-4 text-left flex-grow space-y-4">
+                <li className="flex items-center justify-start">
+                  <Image src={iksz} alt="logo" className="mr-2" />
+                  Admin oldal
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={iksz} alt="logo" className="mr-2" />
+                  Oldal szerkeszthetőség
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Gyors fejlesztési idő
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Tech support
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Egyedi design
+                </li>
+              </ul>
+              <span className="uppercase bg-white text-cyellow font-bold rounded-full p-2 m-4">
+                Érdeklődöm
+              </span>
+            </div>
+          </div>
+
+          <div className="items-center justify-center flex flex-col m-4 border rounded-2xl p-3 h-[539px] w-[270px] bg-gradient-to-br from-customStart to-customEnd">
+            <h4>Normál Weboldal</h4>
+            <p>Normál weboldal, vállalkozásoknak.</p>
+            <div className="border rounded-2xl w-3/4 bg-white mx-2 my-4 p-3 h-[355px] w-[245px] flex flex-col justify-between">
+              <ul className="my-4 text-left flex-grow space-y-4">
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Admin oldal
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Oldal szerkeszthetőség
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Gyors fejlesztési idő
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Tech support
+                </li>
+                <li className="flex items-center justify-start">
+                  <Image src={pipa} alt="logo" className="mr-2" />
+                  Egyedi design
+                </li>
+              </ul>
+              <span className="uppercase bg-white text-cyellow font-bold rounded-full p-2 m-4">
+                Érdeklődöm
+              </span>
+            </div>
+          </div>
+
+          <div className="items-center justify-center flex flex-col m-4 border rounded-2xl p-3 h-[500px] w-[270px]">
+            <h4>Egyedi ötlet</h4>
+            <p>Ha egyedi ötleted van, abban is segítünk</p>
+            <div className="border rounded-2xl bg-cyellow bg-opacity-20 w-3/4 mx-2 my-4 p-3 h-[329px] w-[245px] flex flex-col justify-between">
+              <p className="text-center flex items-center justify-center flex-grow">
+                Lehetőségek kapcsán kérlek érdeklődj!
+              </p>
+              <span className="uppercase bg-white text-cyellow font-bold rounded-full p-2 m-4">
+                Érdeklődöm
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col md:flex-row md:h-[450px] md:mt-0">
+        <div className="w-full md:w-1/2 flex items-center justify-center">
+          <div className="w-full md:w-1/2 flex p-4">
+            <Image
+              src={User}
+              alt="logo"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 flex items-center justify-center p-4">
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold m-3">
+              Rólunk egy pár szó
+            </h3>
+            <p className="text-base md:text-lg m-3 leading-5">
+              Csapatunk 2024ben alakult, jelenleg 2főt számlál. Mögöttünk több
+              év fejlesztésben szerzett tapasztalat. Csapatunk tagjai dolgoztak
+              már egészségügyi szoftvereken,időpont foglaló rendszereken,
+              különböző weboldalakon. Imádjuk a kihívásokat, és a különböző
+              érdekesebbnél érdekesebb projekteket. Ha kérdésed van nyugodtan
+              keress minket
+            </p>
+            <span className="inline-flex items-center border-cyellow border rounded-full justify-center px-3 py-2 md:px-4 md:py-3 mx-3 mt-3 uppercase text-xs md:text-sm text-cyellow font-bold">
+              Kérdésem Van
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col md:flex-row md:h-[450px] bg-cyellow p-10">
+        <div className="w-full md:w-1/2 flex p-4 flex-wrap">
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold m-3">
+              Referenciák
+            </h3>
+            <p className="text-base md:text-lg ml-3 leading-5">
+              Az N&M Web elindulása alkalmából teljesen ingyenes fejlesztési
+              költségi akciót hírdetünk. Célünk, hogy{" "}
+              <span className="text-cyellow">együtt növekedjünk.</span> Így az
+              első 3 ügyfélnek , fejlesztési költségektől mentesen készítünk
+              weboldalt.
+            </p>
+            <p className="text-cyellow font-bold ml-3 text-xl">
+              Ne hagyd ki ezt az egyedi ajánlatot, és kérj ajánlatot még ma!
+            </p>
+          </div>
+          <div className="ml-3 mt-3">
+            <span
+              className={`${lato.className} bg-gradient-to-br from-customStart to-customEnd p-2 rounded-full text-white px-3 py-2 md:px-4 md:py-3 mt-3 uppercase text-xs md:text-sm font-bold`}
+            >
+              Ajánlatot kérek
+            </span>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 flex p-2">
+          <Image
+            src={team}
+            alt="logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
       </div>
 
       {/* <p className={`${lato.className}`}>Text proba1</p>
