a0906028e2073e3d059e34ddf443f26714a31036
badzso-boop <norbert.ujj@gmail.com>
2024-08-08 21:12:11 +0200

    blog post

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -15,18 +15,30 @@ const BlogPost = ({ params }: { params: { slug: string } }) => {
   return (
     <>
       <Header />
-      <div className="container mx-auto p-4 mb-48">
+      <div className="container mx-auto p-4 mb-48 flex flex-col justify-center items-center">
         <div className="flex flex-col items-center w-full">
           <h1 className="text-4xl font-bold mt-4 mb-4">{postData.title}</h1>
         </div>
-        <div className="mt-4 p-4 flex flex-col items-center md:flex-row md:items-start">
-          <Image
-            src={postData.imageSrc}
-            alt={postData.title}
-            width={250}
-            className="rounded-lg mb-4 md:mb-0 md:mr-4"
-          />
-          <p className="text-base">{postData.text}</p>
+        <div className="mt-4 p-4 flex flex-wrap items-center justify-center md:flex-row md:items-start border bg-cyellow bg-opacity-20 drop-shadow-xl w-2/3">
+          <div className="p-3 w-full items-center flex justify-center">
+            <Image
+              src={postData.imageSrc}
+              alt={postData.title}
+              width={250}
+              className="rounded-lg mb-4 md:mb-0 md:mr-4 md:-translate-x-28 md:translate-y-12 z-10"
+            />
+          </div>
+          <div className="md:translate-x-40 w-full md:w-1/3 bg-white rounded-lg p-2">
+            <p className="p-4 text-base">{postData.text}</p>
+            {postData.hashtags.map((hashtag, index) => (
+              <span
+                key={index}
+                className="bg-cyellow rounded-full p-1 m-1 text-xs"
+              >
+                #{hashtag}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
       <Footer />
